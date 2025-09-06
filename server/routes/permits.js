3ce1b33090a32fd64ebda95cad94ea8bdcb82e7d
NbrasY <nbras002@yahoo.com>
2025-09-06 16:36:33 +0300

    Updated permits.js

--- server/routes/permits.js
+++ server/routes/permits.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
 import { supabase } from '../config/database.js';
-import { requirePermission } from '../middleware/auth.js';
+import { authenticateToken, requirePermission } from '../middleware/auth.js';
 
 const router = express.Router();
 
